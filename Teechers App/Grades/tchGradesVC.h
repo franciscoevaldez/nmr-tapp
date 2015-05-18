a514585891e687fb0e5191735a91d0846b8ebdf4
franciscoevaldez <franciscoevaldez@gmail.com>
2015-05-18 13:38:09 -0300

    Evaluaciones Vista 1
    Primer armado de vista

---
+++ Teechers App/Grades/tchGradesVC.h
@@ -0,0 +1,13 @@
+//
+//  tchGradesVC.h
+//  Teechers App
+//
+//  Created by fran on 18/5/15.
+//  Copyright (c) 2015 nmr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tchGradesVC : UIViewController
+
+@end
---
+++ Teechers App/Grades/tchGradesVC.m
@@ -0,0 +1,37 @@
+//
+//  tchGradesVC.m
+//  Teechers App
+//
+//  Created by fran on 18/5/15.
+//  Copyright (c) 2015 nmr. All rights reserved.
+//
+
+#import "tchGradesVC.h"
+
+@interface tchGradesVC ()
+
+@end
+
+@implementation tchGradesVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
